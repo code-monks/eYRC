fe4b0d421f672fc56eae80376d70e03a867ae99e
Nikhil Kumar Mishra <nkmishra1997@gmail.com>
2017-06-11 11:30:56 +0530

    Add files via upload

---
+++ eyrc_hardcode.c
@@ -0,0 +1,357 @@
+/*
+Author List     :Utkarsh Verma, Sagnik Bhowmick, Sahib Majithia, Nikhil Kumar Mishra 
+Filename        :eyrc_hardcode.c
+Theme           :Launch a Module
+Global Variable : data
+*/
+
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+unsigned char data; //to store received data from UDR1
+
+//Configure PORTB 5 pin for servo motor 1 operation
+void servo1_pin_config (void)
+{
+ DDRB  = DDRB | 0x20;  //making PORTB 5 pin output
+ PORTB = PORTB | 0x20; //setting PORTB 5 pin to logic 1
+}
+
+//Configure PORTB 6 pin for servo motor 2 operation
+void servo2_pin_config (void)
+{
+ DDRB  = DDRB | 0x40;  //making PORTB 6 pin output
+ PORTB = PORTB | 0x40; //setting PORTB 6 pin to logic 1
+}
+
+//Configure PORTB 7 pin for servo motor 3 operation
+void servo3_pin_config (void)
+{
+ DDRB  = DDRB | 0x80;  //making PORTB 7 pin output
+ PORTB = PORTB | 0x80; //setting PORTB 7 pin to logic 1
+}
+
+//Function to configure ports to enable robot's motion
+void motion_pin_config (void) 
+{
+ DDRA = DDRA | 0x0F;
+ PORTA = PORTA & 0xF0;
+ DDRL = DDRL | 0x18;   //Setting PL3 and PL4 pins as output for PWM generation
+ PORTL = PORTL | 0x18; //PL3 and PL4 pins are for velocity control using PWM.
+}
+
+void buzzer_pin_config (void)
+{
+	DDRC = DDRC | 0x08;		//Setting PORTC 3 as outpt
+	PORTC = PORTC & 0xF7;		//Setting PORTC 3 logic low to turnoff buzzer
+}
+
+
+
+//Initialize the ports
+void port_init(void)
+{
+ servo1_pin_config(); //Configure PORTB 5 pin for servo motor 1 operation
+ servo2_pin_config(); //Configure PORTB 6 pin for servo motor 2 operation 
+ servo3_pin_config(); //Configure PORTB 7 pin for servo motor 3 operation  
+ motion_pin_config(); //Configure pins required for motion control
+ buzzer_pin_config();
+}
+
+void buzzer_on (void)
+{
+	unsigned char port_restore = 0;
+	port_restore = PINC;
+	port_restore = port_restore | 0x08;
+	PORTC = port_restore;
+}
+
+void buzzer_off (void)
+{
+	unsigned char port_restore = 0;
+	port_restore = PINC;
+	port_restore = port_restore & 0xF7;
+	PORTC = port_restore;
+}
+
+
+//TIMER1 initialization in 10 bit fast PWM mode  
+//prescale:256
+// WGM: 7) PWM 10bit fast, TOP=0x03FF
+// actual value: 42.187Hz 
+void timer1_init(void)
+{
+ TCCR1B = 0x00; //stop
+ TCNT1H = 0xFC; //Counter high value to which OCR1xH value is to be compared with
+ TCNT1L = 0x01;	//Counter low value to which OCR1xH value is to be compared with
+ OCR1AH = 0x03;	//Output compare Register high value for servo 1
+ OCR1AL = 0xFF;	//Output Compare Register low Value For servo 1
+ OCR1BH = 0x03;	//Output compare Register high value for servo 2
+ OCR1BL = 0xFF;	//Output Compare Register low Value For servo 2
+ OCR1CH = 0x03;	//Output compare Register high value for servo 3
+ OCR1CL = 0xFF;	//Output Compare Register low Value For servo 3
+ ICR1H  = 0x03;	
+ ICR1L  = 0xFF;
+ TCCR1A = 0xAB; /*{COM1A1=1, COM1A0=0; COM1B1=1, COM1B0=0; COM1C1=1 COM1C0=0}
+ 					For Overriding normal port functionality to OCRnA outputs.
+				  {WGM11=1, WGM10=1} Along With WGM12 in TCCR1B for Selecting FAST PWM Mode*/
+ TCCR1C = 0x00;
+ TCCR1B = 0x0C; //WGM12=1; CS12=1, CS11=0, CS10=0 (Prescaler=256)
+}
+
+// Timer 5 initialized in PWM mode for velocity control
+// Prescale:64
+// PWM 8bit fast, TOP=0x00FF
+// Timer Frequency:674.988Hz
+void timer5_init()
+{
+	TCCR5B = 0x00;	//Stop
+	TCNT5H = 0xFF;	//Counter higher 8-bit value to which OCR5xH value is compared with
+	TCNT5L = 0x01;	//Counter lower 8-bit value to which OCR5xH value is compared with
+	OCR5AH = 0x00;	//Output compare register high value for Left Motor
+	OCR5AL = 0xFF;	//Output compare register low value for Left Motor
+	OCR5BH = 0x00;	//Output compare register high value for Right Motor
+	OCR5BL = 0xFF;	//Output compare register low value for Right Motor
+	OCR5CH = 0x00;	//Output compare register high value for Motor C1
+	OCR5CL = 0xFF;	//Output compare register low value for Motor C1
+	TCCR5A = 0xA9;	/*{COM5A1=1, COM5A0=0; COM5B1=1, COM5B0=0; COM5C1=1 COM5C0=0}
+ 					  For Overriding normal port functionality to OCRnA outputs.
+				  	  {WGM51=0, WGM50=1} Along With WGM52 in TCCR5B for Selecting FAST PWM 8-bit Mode*/
+	
+	TCCR5B = 0x0B;	//WGM12=1; CS12=0, CS11=1, CS10=1 (Prescaler=64)
+}
+
+//Function To Initialize UART0
+// desired baud rate:9600
+// actual baud rate:9600 (error 0.0%)
+// char size: 8 bit
+// parity: Disabled
+void uart0_init(void)
+{
+	UCSR0B = 0x00; //disable while setting baud rate
+	UCSR0A = 0x00;
+	UCSR0C = 0x06;
+	UBRR0L = 0x5F; //set baud rate lo
+	UBRR0H = 0x00; //set baud rate hi
+	UCSR0B = 0x98;
+}
+
+
+
+// Function for robot velocity control
+void velocity (unsigned char left_motor, unsigned char right_motor)
+{
+	OCR5AL = (unsigned char)left_motor;
+	OCR5BL = (unsigned char)right_motor;
+}
+
+//Function used for setting motor's direction
+void motion_set (unsigned char Direction)
+{
+ unsigned char PortARestore = 0;
+
+ Direction &= 0x0F; 			// removing upper nibbel as it is not needed
+ PortARestore = PORTA; 			// reading the PORTA's original status
+ PortARestore &= 0xF0; 			// setting lower direction nibbel to 0
+ PortARestore |= Direction; 	// adding lower nibbel for direction command and restoring the PORTA status
+ PORTA = PortARestore; 			// setting the command to the port
+}
+
+
+void forward (void) //both wheels forward
+{
+  motion_set(0x06);
+}
+
+void back (void) //both wheels backward
+{
+  motion_set(0x09);
+}
+
+void left (void) //Left wheel backward, Right wheel forward
+{
+  motion_set(0x05);
+}
+
+void right (void) //Left wheel forward, Right wheel backward
+{
+  motion_set(0x0A);
+}
+
+void soft_left (void) //Left wheel stationary, Right wheel forward
+{
+ motion_set(0x04);
+}
+
+void soft_right (void) //Left wheel forward, Right wheel is stationary
+{
+ motion_set(0x02);
+}
+
+void stop (void)
+{
+  motion_set(0x00);
+}
+
+
+//Function to rotate Servo 1 by a specified angle in the multiples of 1.86 degrees
+void servo_1(unsigned char degrees)  
+{
+ float PositionPanServo = 0;
+ PositionPanServo = ((float)degrees / 1.86) + 35.0;
+ OCR1AH = 0x00;
+ OCR1AL = (unsigned char) PositionPanServo;
+}
+
+
+//Function to rotate Servo 2 by a specified angle in the multiples of 1.86 degrees
+void servo_2(unsigned char degrees)
+{
+ float PositionTiltServo = 0;
+ PositionTiltServo = ((float)degrees / 1.86) + 35.0;
+ OCR1BH = 0x00;
+ OCR1BL = (unsigned char) PositionTiltServo;
+}
+
+//Function to rotate Servo 3 by a specified angle in the multiples of 1.86 degrees
+void servo_3(unsigned char degrees)
+{
+ float PositionServo = 0;
+ PositionServo = ((float)degrees / 1.86) + 35.0;
+ OCR1CH = 0x00;
+ OCR1CL = (unsigned char) PositionServo;
+}
+
+//servo_free functions unlocks the servo motors from the any angle 
+//and make them free by giving 100% duty cycle at the PWM. This function can be used to 
+//reduce the power consumption of the motor if it is holding load against the gravity.
+
+void servo_1_free (void) //makes servo 1 free rotating
+{
+ OCR1AH = 0x03; 
+ OCR1AL = 0xFF; //Servo 1 off
+}
+
+void servo_2_free (void) //makes servo 2 free rotating
+{
+ OCR1BH = 0x03;
+ OCR1BL = 0xFF; //Servo 2 off
+}
+
+void servo_3_free (void) //makes servo 3 free rotating
+{
+ OCR1CH = 0x03;
+ OCR1CL = 0xFF; //Servo 3 off
+} 
+
+void object_ungrip (void) // grip the object
+{
+ servo_1 (10);
+ _delay_ms(1000);
+}
+
+void object_grip (void) // relase the object
+{
+ servo_1 (145);
+ _delay_ms(1000);
+
+}
+
+void arm_up (void) // lift the object
+{
+ servo_2 (100);
+ _delay_ms(1000);
+}
+
+void arm_down (void) // bring the object down
+{
+ servo_2 (0);
+ _delay_ms(1000);
+}
+
+SIGNAL(SIG_USART0_RECV) 		// ISR for receive complete interrupt
+{
+	data = UDR0; 				//making copy of data from UDR0 in 'data' variable
+
+	UDR0 = data; 				//echo data back to PC
+
+	if(data == 0x38) //ASCII value of 8
+	{
+		forward();  //forward
+	}
+
+	if(data == 0x32) //ASCII value of 2
+	{
+		back(); //back
+	}
+
+	if(data == 0x34) //ASCII value of 4
+	{
+		left();  //left
+	}
+
+	if(data == 0x36) //ASCII value of 6
+	{
+		right(); //right
+	}
+
+	if(data == 0x35) //ASCII value of 5
+	{
+		stop(); //stop
+	}
+
+	if(data == 0x37) //ASCII value of 7
+	{
+		buzzer_on();
+	}
+
+	if(data == 0x39) //ASCII value of 9
+	{
+		buzzer_off();
+	}
+	
+	if(data == 0x30) //ASCII value of 0
+	{
+		object_grip();
+	}
+
+	if(data == 0x2E) //ASCII value of .
+	{
+		object_ungrip();
+	}
+
+	if(data == 0x2D) //ASCII value of -
+	{
+		arm_up();
+	}
+	
+	if(data == 0x2B) //ASCII value of +
+	{
+		arm_down();
+	}
+
+
+}
+
+
+//Function to initialize all the peripherals
+void init_devices(void)
+{
+	cli(); //disable all interrupts
+	port_init();
+	uart0_init();
+	timer1_init();
+	timer5_init();
+	sei(); //re-enable interrupts
+}
+
+//Main function
+int main(void)
+{
+ init_devices();
+ 	
+ while (1);
+
+}
